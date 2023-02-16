cbe0fe060da700b841d6be7a016170c13d1c32dc
Vijay Anand <vjyanand@users.noreply.github.com>
2023-02-16 12:42:23 +0530

    Update a.js

--- a.js
+++ a.js
@@ -1,1 +1,5 @@
-document.write("Hello")
+var elemDiv = document.createElement('div');
+
+elemDiv.style.cssText = 'position:absolute;width:100%;height:100%;opacity:0.3;z-index:100;background:#000;';
+
+document.body.appendChild(elemDiv);
